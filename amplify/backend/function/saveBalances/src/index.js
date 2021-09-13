a6c6d10eb496141da0108c60ece08041f66fcdbb
BROCHARD, Xavier Alexandre <xavier-alexandre.brochard@airbus.com>
2021-09-14 00:17:55 +0200

    Save balances using the GraphQL API

--- amplify/backend/function/saveBalances/src/index.js
+++ amplify/backend/function/saveBalances/src/index.js
@@ -10,25 +10,81 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
+const axios = require("axios");
+const gql = require("graphql-tag");
+const graphql = require("graphql");
+const { print } = graphql;
+
 const getAutoFarmBalance = require("./getAutoFarmBalance");
 const getIronFinanceBorrowBalance = require("./getIronFinanceBorrowBalance");
 
 exports.handler = async (event) => {
-  // TODO implement
+  try {
+    const autofarmBalance = await getAutoFarmBalance();
+    const ironfinanceBalance = await getIronFinanceBorrowBalance();
+    console.log("autofarmBalance", autofarmBalance);
+    console.log("ironfinanceBalance", ironfinanceBalance);
 
-  const autofarmBalance = await getAutoFarmBalance();
-  const ironfinanceBalance = await getIronFinanceBorrowBalance();
-  console.log("autofarmBalance", autofarmBalance);
-  console.log("ironfinanceBalance", ironfinanceBalance);
+    const createAutofarmBalance = gql`
+      mutation createAutofarmBalance {
+        createAutofarmBalance(
+          input: { date: "${new Date().toISOString()}", balance: ${autofarmBalance} }
+        ) {
+          id
+          date
+          balance
+          createdAt
+          updatedAt
+        }
+      }
+    `;
 
-  const response = {
-    statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-    body: JSON.stringify("Hello from Lambda!"),
-  };
-  return response;
+    // Save Autofarm balance
+    await axios({
+      url: process.env.API_AUTOFARMCHECKER_GRAPHQLAPIENDPOINTOUTPUT,
+      method: "post",
+      headers: {
+        "x-api-key": process.env.API_AUTOFARMCHECKER_GRAPHQLAPIKEYOUTPUT,
+      },
+      data: {
+        query: print(createAutofarmBalance),
+      },
+    });
+
+    const createIronfinanceBalance = gql`
+      mutation createIronfinanceBalance {
+        createIronfinanceBalance(
+          input: { date: "${new Date().toISOString()}", balance: ${ironfinanceBalance} }
+        ) {
+          id
+          date
+          balance
+          createdAt
+          updatedAt
+        }
+      }
+    `;
+
+    // Save IronFinance balance
+    await axios({
+      url: process.env.API_AUTOFARMCHECKER_GRAPHQLAPIENDPOINTOUTPUT,
+      method: "post",
+      headers: {
+        "x-api-key": process.env.API_AUTOFARMCHECKER_GRAPHQLAPIKEYOUTPUT,
+      },
+      data: {
+        query: print(createIronfinanceBalance),
+      },
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify("Balances successfully saved!"),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+  } catch (err) {
+    console.log("error posting to appsync: ", err);
+  }
 };
